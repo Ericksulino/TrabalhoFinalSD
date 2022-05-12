12d656544cca2271c0b1090307b389a23320a20b
Ericksulino <ericksulino@gmail.com>
2022-05-12 18:56:25 -0300

    alteraçoes

--- cliente.py
+++ cliente.py
@@ -1,34 +1,12 @@
-from socket import *
-import time
+import socket
+ip = 'localhost'
+port = 8001
+addr = ((ip,port))
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(addr)
 
-serverHost = 'localhost'
-serverPort = 50007
 
-# Adotei o caractere \n como fim de mensagem
-mensagem = [b'Ola mundo da internet!\n', b'bla\n', b'ble\n']
 
-sockobj = socket(AF_INET, SOCK_STREAM)
-sockobj.connect((serverHost, serverPort))
-
-for linha in mensagem:
-    time.sleep(1)
-
-    while linha:
-        enviado = sockobj.send(linha)
-        if enviado == 0:
-            print("Servidor fechou")
-            break
-        # corta parte da linha ja enviada
-        linha = linha[enviado:]
-
-    data = b''
-    # Adotei o caractere \n como fim de mensagem
-    while b'\n' not in data: 
-         newdata = sockobj.recv(1024)
-         if not newdata:
-             print('Servidor fechou conexao')
-             break
-         data += newdata
-    print('Cliente recebeu:', data)
-
-sockobj.close()
+msg = input("Digite a msg: ")
+client_socket.send(msg)
+#extr = client_socket.recv(1024).decode()
