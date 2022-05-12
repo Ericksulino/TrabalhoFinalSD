4c4ac74ca5edc026f50e66c971be11f86bd2ac43
Ericksulino <ericksulino@gmail.com>
2022-05-12 19:27:43 -0300

    alteraçoes

--- cliente.py
+++ cliente.py
@@ -5,8 +5,13 @@ addr = ((ip,port))
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(addr)
 
+mensagem = ''
 
+while(mensagem != 'sair'):
+    mensagem = input('Digite uma mensagem para enviar ao servidor:')
+    client_socket.send(mensagem.encode())
+    #print('Mensagem enviada!')
+    print('Mensagem recebida:' + client_socket.recv(1024).decode())
 
-msg = input("Digite a msg: ")
-client_socket.send(msg)
-#extr = client_socket.recv(1024).decode()
+print('Saindo...')
+client_socket.close()
