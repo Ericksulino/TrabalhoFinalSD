cd8a450fd8fa86ba4458ef1c99bccf086037a532
Ericksulino <ericksulino@gmail.com>
2022-05-12 19:27:34 -0300

    alteraçoes

--- servidor.py
+++ servidor.py
@@ -1,5 +1,14 @@
 import socket
 
+def cria_matriz(lst, n):  
+    for i in range(0, len(lst), n): 
+        yield lst[i:i + n] 
+
+def StrToInt(txt):
+    stlist = txt.split(',')
+    lista = list(map(int, stlist))
+    return lista
+
 host = 'localhost'
 port = 8001
 addr = ((host,port))
@@ -18,7 +27,19 @@ running = True
 while(running):
     recebe = con.recv(1024)
     print('mensagem recebida: '+ recebe.decode())
-    msg = recebe.decode().split(',')
+    msg = recebe.decode().split('/')
     if recebe.decode() == 'sair':
         running = False
+    elif recebe.decode() == None:
+        con.send('erro'.encode())
+    
+    else:
+        n1 = msg[0]
+        n2 = msg[1]
+        list1 = StrToInt(msg[2])
+        list2 = StrToInt(msg[3])
+        matriz1 = list(cria_matriz(list1, n1))
+        matriz2 = list(cria_matriz(list2, n2))
+        con.send('Matrizes: '+matriz1+'\n'+matriz2+'\n'.encode())
+
 serv_socket.close()
